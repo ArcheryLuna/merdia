d2beb3398728e842b662f17b57c1b1688a7330f3
ArcheryLuna <fazearchery@outlook.com>
2022-10-30 20:23:57 +0000

    New Files

---
+++ src/components/stress-management/photo-gallary/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import "./index.css"
+
+
+
+export default class PhotoGallery extends Component {
+    render() {
+      return (
+          <>
+            <div className='gallery-container'>
+            
+            </div>
+          </>
+      )
+    }
+}
