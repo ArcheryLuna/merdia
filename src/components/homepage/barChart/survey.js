bd6aeb406d0cc98e0ee3927d029bf30cf4de4356
Archery Luna <76516425+ArcheryLuna@users.noreply.github.com>
2022-12-06 00:09:32 +0000

    Api fix servey

--- src/components/homepage/barChart/survey.js
+++ src/components/homepage/barChart/survey.js
@@ -8,7 +8,7 @@ export default function Survey() {
         function UpdatedData() {
             if (dataUpdated) {
                 return <div>
-                    <div class="fixed bottom-1 md:bottom-5 left-5 bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md" role="alert">
+                    <div class="fixed bottom-1 md:bottom-5 left-1 md:left-5 bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md" role="alert">
                         <div class="flex">
                             <div class="py-1"><svg class="fill-current h-6 w-6 text-teal-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
                             <div>
@@ -38,7 +38,7 @@ export default function Survey() {
                 headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
                 body: JSON.stringify({ attentivness: attentiveChecked, performance: performanceChecked, depression: depressedChecked, anxiety: anxietyChecked })
             };
-            fetch('http://node1.lunacs.co.uk:7321/api/v1/table', requestOptions)
+            fetch('https://media-api.archery-luna.com/api/v1/table', requestOptions)
                 .then(response => response.json())
                 .then(data => {
                     console.log(data.message)
