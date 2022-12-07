198675ff4b8233fee5600ca464c1c15735a8a91e
Archery Luna <76516425+ArcheryLuna@users.noreply.github.com>
2022-12-07 11:41:23 +0000

    Update Survey.js

--- src/components/homepage/barChart/survey.js
+++ src/components/homepage/barChart/survey.js
@@ -89,7 +89,7 @@ export default function Survey() {
                         transition={{ duration: 0.5 }}
                         whileInView={{ opacity: 1 }}
                         >
-                            <h1 className='text-xl text-center md:text-left xl:text-2xl mb-0 md:mb-5 mt-0 md:mt-16'><i className="fa-regular fa-clipboard"/> Would you like to participate in a survey?</h1>
+                            <h1 className='text-xl text-center md:text-left xl:text-2xl mb-3.5 md:mb-5 mt-0 md:mt-16'><i className="fa-regular fa-clipboard"/> Would you like to participate in a survey?</h1>
                             <p>Please help us gain more accurate information by participating in the survey. The awnsers you submit are completely anonymous.</p>
                         </motion.div>
                     <motion.div
